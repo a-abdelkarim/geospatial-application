353526870541c83eb3d9085a08cc5f66e88624df
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-10-10 11:47:09 +0300

    automate creation of temp DIR

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, send_from_directory
 from flask_restful import Resource, Api
 
@@ -10,6 +11,9 @@ api = Api(app)
 
 # Define the path to your static files (e.g., index.html).
 static_files_dir = 'static'
+TEMP_DIR = 'temp'
+
+os.makedirs(TEMP_DIR, exist_ok=True)
 
 @app.route('/')
 def index():
@@ -20,10 +24,10 @@ class UploadShapefile(Resource):
     def post(self):
         try:
             uploaded_file = request.files['shapefile']
-            uploaded_file.save('temp/temp.zip')
-            shapefile_processor = ShapefileProcessor('temp/temp.zip')
+            uploaded_file.save(TEMP_DIR + '/temp.zip')
+            shapefile_processor = ShapefileProcessor(TEMP_DIR + '/temp.zip')
             shapefile_geometry = shapefile_processor.check_geometry()
-            print(shapefile_geometry)
+
             if shapefile_geometry != "Polygon":
                 shapefile_processor.clean_temp_directory()
                 return {'message': "The shapefile must be Polygon!", "data": None}, 422
